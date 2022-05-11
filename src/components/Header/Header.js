0ec775c08a8ac2fc04f8ab68ec6e690e0c2c7b36
r0bomurlok <igrtnsk@ya.ru>
2022-05-11 16:08:00 +0300

    header fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,50 @@
 import './Header.css';
 
 import logoPath from '../../images/header/logo.png';
-//import menuLogoPath from '../../images/header/menu.svg';
+import menuLogoPath from '../../images/header/menu.svg';
 
 import React from "react";
 import {Link} from "react-router-dom";
 import ProfileButton from "../ProfileButton/ProfileButton";
 
 function Header(props) {
-
+  const {currentPage, onMenuClick} = props;
   return (
     <header className="header">
-      <img className="header__logo" src={logoPath} alt="Лого"/>
 
       {props.isLoggedIn
         ?
         <>
           <div className="header__links">
-            <div  className="header__movie-link">
-            <Link to="/movies" className="header__link"> Фильмы </Link>
-            <Link to="/saved-movies" className="header__link"> Сохраненные фильмы </Link>
-          </div>
-
+            <Link to="/" className="header__logo-link">
+              <img className="header__logo" src={logoPath} alt="Лого"/>
+            </Link>
+            <div className="header__link-container">
+              <Link to="/movies"
+                    className={currentPage === 'movies' ? "header__link header__link_active" : "header__link"}>
+                Фильмы
+              </Link>
+              <Link to="/saved-movies"
+                    className={currentPage === 'saved-movies' ? "header__link header__link_active" : "header__link"}>
+                Сохраненные фильмы
+              </Link>
+            </div>
             <ProfileButton/>
           </div>
-
+          <button className="header__menu-button" onClick={onMenuClick}>
+            <img className="header__menu-logo" src={menuLogoPath} alt="Кнопка меню"/>
+          </button>
         </>
         :
         <div className="header__creds">
           <Link to="/sign-up" className="header__link"> Регистрация </Link>
-          <button className="header__auth-button"> Войти</button>
+          <Link to="/sign-in">
+            <button className="header__auth-button"> Войти</button>
+          </Link>
         </div>
       }
     </header>
   );
 }
 
-export default Header;
+export default Header
